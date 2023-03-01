8d7a6b6f3d97f2734698e301729f4ad66ce15339
Kombobill <118278282+Kombobill@users.noreply.github.com>
2023-03-01 12:01:18 +0300

    interacting with database

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,19 +2,15 @@ class User < ActiveRecord::Base
     has_many :reviews
     has_many :products, through: :reviews
     
-    def reviews
-      # code for returning all reviews given by this user
-    end
     
-    def products
-      # code for returning all products reviewed by this user
-    end
     
     def favorite_product
+      products.order("reviews.star_rating DESC").first
       # code for returning the product instance that has the highest rating from this user
     end
     
     def remove_reviews(product)
+      reviews.where(product_id: product.id).destroy_all
       # code for removing all reviews for a specific product by this user
     end
 end
