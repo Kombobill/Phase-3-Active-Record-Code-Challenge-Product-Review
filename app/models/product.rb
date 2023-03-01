eea4b08d994076c909607999837b502d5b8147c8
Kombobill <118278282+Kombobill@users.noreply.github.com>
2023-03-01 11:46:35 +0300

    creating and managing product objects

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,23 +2,26 @@ class Product < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
     
-    def reviews
-      # code for returning all reviews for this product
-    end
     
-    def users
-      # code for returning all users who reviewed this product
-    end
-    
-    def leave_review(user, star_rating, comment)
+    def leave_review(user, star_rating, comment)Review.create(
+      comment: comment,
+      star_rating: star_rating,
+      product_id: self.id,
+      user_id: user.id
+  )
       # code for creating a new review associated with this product and the user
     end
     
     def print_all_reviews
+      all_reviews = self.reviews
+      all_reviews.collect do |review|
+          review.print_review
+      end 
       # code for printing all reviews for this product
     end
     
     def average_rating
+      reviews.average(:star_rating)
       # code for returning the average rating of all reviews for this product
     end
 end
