9cc83adac6ca9f164a320eb1e0fa77569302f39e
Kombobill <118278282+Kombobill@users.noreply.github.com>
2023-03-01 12:15:53 +0300

    seeding database initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,18 @@
-# seeds.rb
+require_relative '../config/environment'
+puts "🌱 Seeding data..."
 
 # Create some products
-Product.create(name: "Widget A")
-Product.create(name: "Widget B")
-Product.create(name: "Widget C")
+product1= Product.create(name: "Widget A")
+product2= Product.create(name: "Widget B")
+product3= Product.create(name: "Widget C")
 
 # Create some users
-User.create(name: "Alice")
-User.create(name: "Bob")
-User.create(name: "Charlie")
+user1= User.create(name: "Alice")
+user2= User.create(name: "Bob")
+user3= User.create(name: "Charlie")
 
 # Create some reviews
-Review.create(product_id: 1, user_id: 1, star_rating: 4, comment: "Great product!")
-Review.create(product_id: 1, user_id: 2, star_rating: 3, comment: "Average product.")
-Review.create(product_id: 2, user_id: 1, star_rating: 5, comment: "Amazing product!!!")
-Review.create(product_id: 2, user_id: 2, star_rating: 4, comment: "Really good product.")
+review1= Review.create(product_id: 1, user_id: 1, star_rating: 4, comment: "Great product!")
+review2= Review.create(product_id: 1, user_id: 2, star_rating: 3, comment: "Average product.")
+review3= Review.create(product_id: 2, user_id: 1, star_rating: 5, comment: "Amazing product!!!")
+review4= Review.create(product_id: 2, user_id: 2, star_rating: 4, comment: "Really good product.")
